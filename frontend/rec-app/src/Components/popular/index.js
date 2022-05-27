f2dba77254153d5003aa0e5f032d228667f9b76b
Debarghya Mondal <debarghyamondal15@gmail.com>
2022-05-27 22:22:00 +0530

    add random songs recommendation

--- frontend/rec-app/src/Components/popular/index.js
+++ frontend/rec-app/src/Components/popular/index.js
@@ -5,15 +5,27 @@ import "../../App.css";
 
 function Popular(props) {
   const [popularSongs, setPopularSongs] = useState([]);
+  const [randomSongs, setRandomSongs] = useState([]);
   async function getPopularSongs() {
     const { data } = await supabase
       .from("song_recommendations")
       .select("song_id")
       .order("searches_count", { ascending: false })
-      .limit(10);
+      .limit(5);
     const songIds = getSongId(data);
-    getSpotifyData(songIds);
+    getSpotifyData(songIds, setPopularSongs);
   }
+
+  async function getRandomSongs() {
+    let { data, error } = await supabase.rpc("get_random_song_ids");
+
+    if (error) console.error(error);
+    else {
+      const songIds = getSongId(data);
+      getSpotifyData(songIds, setRandomSongs);
+    }
+  }
+
   function getSongId(data) {
     const idArray = [];
     for (let index = 0; index < data.length; index++) {
@@ -22,7 +34,8 @@ function Popular(props) {
     }
     return idArray;
   }
-  function getSpotifyData(songIdsArray) {
+
+  function getSpotifyData(songIdsArray, updateState) {
     fetch(`${process.env.REACT_APP_SERVER_URL}popular`, {
       method: "POST",
       body: JSON.stringify({
@@ -31,17 +44,35 @@ function Popular(props) {
     })
       .then(resp => resp.json())
       .then(data => {
-        setPopularSongs(data.tracks);
+        updateState(data.tracks);
+        console.log(data.tracks);
       });
   }
+
   useEffect(() => {
     getPopularSongs();
+    getRandomSongs();
   }, []);
+
   return (
     <div className='popularContainer'>
-      <h1 className='popularHeader'>Popular Now</h1>
+      <h1 className='popularHeader'>Popular now</h1>
       <div className='popularFlex'>
-        {popularSongs.map((song, index) => {
+        {popularSongs.map(song => {
+          return (
+            <PopularSongs
+              key={song.id}
+              song={song}
+              setOption={props.setOption}
+              setSongs={props.setSongs}
+            />
+          );
+        })}
+      </div>
+
+      <h1 className='popularHeader'>Try out new songs</h1>
+      <div className='popularFlex'>
+        {randomSongs.map(song => {
           return (
             <PopularSongs
               key={song.id}
